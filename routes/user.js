a2962b8300baa9b43143300e03c449b595217f67
Nitish <nk301998@gmail.com>
2023-04-28 14:51:09 +0530

    added follower and following property in the schema of the user, and created route for follow and unfollow

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const POST = mongoose.model("POST")
 const USER = mongoose.model("USER");
+const requireLogin = require('../middlewares/requireLogin')
 
 
 router.get("/user/:id", (req, res)=>{
@@ -18,5 +19,40 @@ router.get("/user/:id", (req, res)=>{
     .catch(err => res.status(422).json({error: "User not found"}))
 })
 
+// to follow user
+router.put("/follow", requireLogin, (req, res)=>{
+    USER.findByIdAndUpdate(req.body.followId, {
+        $push: {followers: req.user._id}
+    },{
+        new: true
+    }).then((result)=>{
+        USER.findByIdAndUpdate(req.user._id,{
+            $push: {following: req.body.followId}
+        },{
+            new: true
+        }).then(result => res.json(result))
+        .catch(err => {return res.status(422).json({error: err})})
+    }).catch((err)=> {
+        return res.status(422).json({error: err})
+    })
+})
+
+// to unfollow user
+router.put("/unfollow", requireLogin, (req, res)=>{
+    USER.findByIdAndUpdate(req.body.followId, {
+        $pull: {followers: req.user._id}
+    },{
+        new: true
+    }).then((result)=>{
+        USER.findByIdAndUpdate(req.user._id,{
+            $pull: {following: req.body.followId}
+        },{
+            new: true
+        }).then(result => res.json(result))
+        .catch(err => {return res.status(422).json({error: err})})
+    }).catch((err)=> {
+        return res.status(422).json({error: err})
+    })
+})
 
 module.exports = router  
